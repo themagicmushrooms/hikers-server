f632583717acd0425d2907f439af7abe72ff6c84
Seb Emonet <seb@cygnus.(none)>
2013-09-28 15:43:17 +0200

    Fix test

--- tests/test_non_generic_api.py
+++ tests/test_non_generic_api.py
@@ -452,7 +452,7 @@ class NonGenericAPITestAuthenticated(APITestCase):
         }
         self.client.force_authenticate(user=note.hike.owner)
         response = self.client.put(url, data)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_put_note_invalid_position(self):
         note = NoteFactory.create()
